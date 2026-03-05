1053088154acb7142785cac4bee5a60c70113d54
Louis Declerck <declerck.louis@outlook.com>
2026-03-05 09:35:53 +0100

    test to buildwith ultralytics improvements for SEO

---
+++ postprocess.py
@@ -0,0 +1,19 @@
+# https://pypi.org/project/mkdocs-ultralytics-plugin
+from plugin import postprocess_site
+
+if __name__ == "__main__":
+    postprocess_site(
+        site_dir="site",  # Your build output directory
+        docs_dir="docs",  # Your source docs directory
+        site_url="https://lab.packetflow.be",
+        default_image="",
+        default_author="declerck.louis@outlook.com",
+        add_desc=True,
+        add_image=True,
+        add_authors=True,
+        add_json_ld=True,
+        add_share_buttons=True,
+        add_css=True,
+        add_copy_llm=True,
+        verbose=True,
+    )
